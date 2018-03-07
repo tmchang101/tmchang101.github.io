271b9d63cf99dbfe25ed9c6b035a771436222c19
tmchang101 <36144833+tmchang101@users.noreply.github.com>
2018-03-07 16:30:33 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-    alert('---- change images. ---'); 
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/CS3_icon.png');
+      myImage.setAttribute ('src','images/class.jpg');
     } else {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }

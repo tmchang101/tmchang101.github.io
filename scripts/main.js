26febec4faa34617bcdecf905504d8b5323d2144
tmchang101 <36144833+tmchang101@users.noreply.github.com>
2018-03-07 15:50:40 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,11 @@
+var myImage = document.querySelector('img');
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('div');
-
-alert('---1----'); 
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  alert('--- set new content. ---'); 
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-}
-
-/*
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-
-*/
-
-myButton.onclick = function() {
-  setUserName();
+myImage.onclick = function() {
+    alert('---- change images. ---'); 
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/CS3_icon.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
 }
